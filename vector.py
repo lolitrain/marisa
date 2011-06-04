6b21958584737b7137cef53de982f9ecf76b6502
Maxim Siniavine <m.siniavine@gmail.com>
2011-06-04 19:54:42 -0400

    Made vector slightly faster

--- vector.py
+++ vector.py
@@ -8,7 +8,7 @@ class vector:
         return vector(tuple((x-y for x,y in zip(self.vec, other.vec))))
 
     def __add__(self, other):
-        return vector(tuple((x+y for x,y in zip(self.vec, other.vec))))
+        return vector((self.vec[0] + other.vec[0], self.vec[1]+other.vec[1]))
 
     def dot(self, other):
         return math.sqrt(sum((x*y for x,y in zip(self.vec, other.vec))))
@@ -21,7 +21,7 @@ class vector:
         return vector(tuple((x/length for x in self.vec)))
 
     def __mul__(self, n):
-        return vector(tuple((x*n for x in self.vec)))
+        return vector([x*n for x in self.vec])
 
     def __str__(self):
         return str(self.vec)
