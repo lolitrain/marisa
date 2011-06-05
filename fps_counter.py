c73b5dd0eedad6eccb422c8a6a7dbccce2fd25c7
Maxim Siniavine <m.siniavine@gmail.com>
2011-06-05 16:00:33 -0400

    Added a counter that show how many milliseconds it takes to do a frame

--- fps_counter.py
+++ fps_counter.py
@@ -12,9 +12,11 @@ class FpsCounter:
     def do_frame(self):
         while True:
             self.fps = timer.fps()
+            self.frame_time = timer.frame_time
             for w in timer.wait(1000):
                 yield
 
     def draw(self):
         self.font.show_text((0, 600-16), "%f" % self.fps)
+        self.font.show_text((0, 600-32), "%d" % self.frame_time)
 
